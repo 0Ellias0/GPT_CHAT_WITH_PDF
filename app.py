04718e185973d0cac8404056da4c5d89fddd1d4e
Hebert Palmera <hpalmerabuelvas@gmail.com>
2023-07-23 10:32:29 -0500

    API key added

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st 
+from dotenv import load_dotenv
 
 
 
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDfs", page_icon=":books:")
     
     st.header("Chat with multiple PDfs :books:")

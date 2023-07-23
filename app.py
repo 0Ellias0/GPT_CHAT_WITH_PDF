f92fe4f93d798c247f1c79f4f10bcf0c9060d4a6
Hebert Palmera <hpalmerabuelvas@gmail.com>
2023-07-22 23:41:24 -0500

    interface created with streamlit

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st 
+
+
+
+
+def main():
+    st.set_page_config(page_title="Chat with multiple PDfs", page_icon=":books:")
+    
+    st.header("Chat with multiple PDfs :books:")
+    st.text_input("Ask a question about your documments:")
+    
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        st.file_uploader("Upload your PDFs here and press click on 'Process'")
+        st.button("Process")
+
+
+
+if __name__ == '__main__':
+    main()

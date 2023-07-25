adb41be2acca94929b1a96e04d04ab6448267e0b
Hebert Palmera <hpalmerabuelvas@gmail.com>
2023-07-24 20:12:00 -0500

    session state conversation added

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 
 
 def get_pdf_text(pdf_docs):
@@ -30,12 +33,25 @@ def get_vectorstore(text_chunks):
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
 
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDfs", page_icon=":books:")
     
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
     st.header("Chat with multiple PDfs :books:")
     st.text_input("Ask a question about your documments:")
     
@@ -56,6 +72,9 @@ def main():
 
                 #Create Text base store 
                 vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain 
+                st.session_state.conversation = get_conversation_chain(vectorstore)
 
 if __name__ == '__main__':
     main()
